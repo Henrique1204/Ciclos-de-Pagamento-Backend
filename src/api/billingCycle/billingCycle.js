61cda59cd2d09695c1f2ae8eeb293e4d0861cebd
Paulo <pauloh16.pdr@gmail.com>
2021-01-10 17:40:41 -0300

    Adicionar menssagens de erros personalizadas

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -2,20 +2,20 @@ const restful = require("node-restful");
 const mongoose = restful.mongoose;
 
 const creditSchema =  new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true }
+    name: { type: String, required: [true, "É necessário informar o nome do crédito."] },
+    value: { type: Number, min: 0, required: [true, "É necessário informar o valor do crédito."] }
 });
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    name: { type: String, required: [true, "É necessário informar o nome do débito."] },
+    value: { type: Number, min: 0, required: [true, "É necessário informar o valor do débito."] },
     status: { type: String, required: false, uppercase: true, enum: ["PAGO", "PENDENTE", "AGENDADO"] }
 });
 
 const billingCyclesSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    month: { type: Number, min: 1, max: 12, required: true },
-    year: { type: Number, min: 1970, max: 2100, required: true },
+    name: { type: String, required: [true, "É necessário informar o nome do ciclo de pagamento."] },
+    month: { type: Number, min: 1, max: 12, required: [true, "É necessário informar o mês."] },
+    year: { type: Number, min: 1970, max: 2100, required: [true, "É necessário informar o ano."] },
     credits: [creditSchema],
     debts: [debtSchema]
 });
