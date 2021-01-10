db6b7c0e59594925c104415d31ca5dab9f457a9a
Paulo <pauloh16.pdr@gmail.com>
2021-01-10 17:57:19 -0300

    Adicionar rota buscar numeros de dados

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -15,4 +15,14 @@ BillingCycle.route('get', (req, res, next) => {
     });
 });
 
+BillingCycle.route("count", (req, res, next) => {
+    BillingCycle.count((erro, value) => {
+        if (erro) {
+            res.status(500).json({ errors: [erro] });
+        } else {
+            res.json({ value });
+        }
+    });
+});
+
 module.exports = BillingCycle;
