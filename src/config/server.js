944183969c7d893fcacd0ecde36966a4228bd6bd
Paulo <pauloh16.pdr@gmail.com>
2021-01-10 23:19:25 -0300

    Permitir acesso pelo cors

--- src/config/server.js
+++ src/config/server.js
@@ -2,9 +2,11 @@ const port = 3003;
 
 // Middlewares
 const bodyParser = require("body-parser");
+const allowCors = require("./cors.js");
 const express = require("express");
 const server = express();
 
+server.use(allowCors());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
