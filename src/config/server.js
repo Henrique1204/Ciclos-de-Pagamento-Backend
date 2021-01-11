12002bfbb42e451899adac00fffc496bc5e4b8ed
Paulo <pauloh16.pdr@gmail.com>
2021-01-10 23:23:01 -0300

    Corrigir problema no cors

--- src/config/server.js
+++ src/config/server.js
@@ -6,7 +6,7 @@ const allowCors = require("./cors.js");
 const express = require("express");
 const server = express();
 
-server.use(allowCors());
+server.use(allowCors);
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
