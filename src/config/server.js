5cbf771e0ba412d737b2ec303ef880d5156060d9
Paulo <pauloh16.pdr@gmail.com>
2021-01-10 23:32:16 -0300

    Implementar paginacao

--- src/config/server.js
+++ src/config/server.js
@@ -1,14 +1,16 @@
 const port = 3003;
 
 // Middlewares
-const bodyParser = require("body-parser");
 const allowCors = require("./cors.js");
+const bodyParser = require("body-parser");
+const queryParse = require("express-query-int");
 const express = require("express");
 const server = express();
 
 server.use(allowCors);
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(queryParse());
 
 server.listen(port, () => console.log(`Servidor rodando na porta ${port}.`));
 
