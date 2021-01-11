690836744992adc861abb495080283c5c00183d6
Paulo <pauloh16.pdr@gmail.com>
2021-01-10 21:17:13 -0300

    Atualizar biblioteca mongoose

--- src/config/database.js
+++ src/config/database.js
@@ -5,4 +5,4 @@ mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limit
 mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite mínimo de '{MAX}'.";
 mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'.";
 
-module.exports = mongoose.connect("mongodb://localhost/mymoney");
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true });
