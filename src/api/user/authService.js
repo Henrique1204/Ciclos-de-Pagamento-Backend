f3d663fe09560c46ad559301d49be39d379ad4df
Paulo <pauloh16.pdr@gmail.com>
2021-01-23 11:30:39 -0300

    Implementar autenticacao com JWT

---
+++ src/api/user/authService.js
@@ -0,0 +1,88 @@
+const _ = require("lodash");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("./user.js");
+const env = require("../../.env");
+
+const emailRegex = /\S+@\S+\.\S+/;
+const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/;
+
+const sendErrosFromDB = (res, dbErrors) => {
+    const errors = [];
+
+    _.forIn(dbErrors.errors, error => errors.push(error.message));
+
+    return res.status(400).json({ errors });
+};
+
+const login = (req, res) => {
+    const email = req.body.email || "";
+    const password = req.body.password || "";
+
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return sendErrosFromDB(res, err);
+        } else if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign(user, env.authSecret, {
+                expiresIn: "1 day"
+            });
+
+            const { name, email } = user;
+            res.json({ name, email, token });
+        } else {
+            return res.status(400).send({ errors: ["Usuário/senha inválidos"] });
+        }
+    });
+};
+
+const validarToken = (req, res) => {
+    const token = req.body.token || "";
+
+    jwt.verify(token, env.authSecret, (err, decoded) => res.status(200).send({ valid: !err }));
+};
+
+const signUp = (req, res) => {
+    const name = req.body.name || "";
+    const email = req.body.email || "";
+    const password = req.body.password || "";
+    const confirmPassword = req.body.confirm_password || "";
+
+    if (!email.match(emailRegex)) {
+        return res.status(400).send({ errors: ["O e-mail informado está inválido"] });
+    }
+
+    if (!password.match(passwordRegex)) {
+        return res.status(400).send({
+            errors: [
+                "Senha precisa ter: uma letra maiúscula, uma letra minúscula, um número, um caractere especial e ter de 6 - 20 caracteres"
+            ]
+        });
+    }
+
+    const salt = bcrypt.genSaltSync();
+    const passwordHash = bcrypt.hashSync(password, salt);
+
+    if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
+        return res.status(400).send({ errors: ["Senhas não conferem"] });
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return sendErrosFromDB(res, err);
+        } else if (user) {
+            return res.status(400).send({ errors: ["Usário já cadastrado."] });
+        } else {
+            const newUser = new User({ name, email, password: passwordHash });
+
+            newUser.save((err) => {
+                if (err) {
+                    return sendErrosFromDB(res, err);
+                } else {
+                    login(req, res);
+                }
+            });
+        }
+    });
+};
+
+module.exports = { login, signUp, validarToken };
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const auth = require("./auth.js");
 
 module.exports = (server) => {
+    // Rotas fechadas.
+    const protectedApi = express.Router();
+    server.use("/api", protectedApi);
+
+    protectedApi.use(auth);
+
     // Definir URL base para as rotas.
     const router = express.Router();
     server.use("/api", router);
@@ -8,5 +15,13 @@ module.exports = (server) => {
     // Rotas de Ciclo de Pagamento.
     const BillingCycles = require("../api/billingCycle/billingCycleService.js");
     BillingCycles.register(router, "/billingCycles");
-}
 
+    // Rotas Abertas.
+    const openApi = express.Router();
+    server.use("/opai", openApi);
+
+    const AuthService = require("../api/user/authService.js");
+    openApi.post("/login", AuthService.login);
+    openApi.post("/signup", AuthService.signUp);
+    openApi.post("/validarToken", AuthService.validarToken);
+}
